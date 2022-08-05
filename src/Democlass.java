f0892fd770126997978af84933c7107667cedb9b
vivek-viki <Mukunda.Vivek@ivycomptech.com>
2022-08-05 16:42:47 +0530

    commented unwnated

--- src/Democlass.java
+++ src/Democlass.java
@@ -2,7 +2,7 @@ public class Democlass {
     public static void main(String args[])
     {
         System.out.println("hi");
-        System.out.println("hi1");
+        /*System.out.println("hi1");*/
     }
 }
 
